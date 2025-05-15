e6fec7e5a7675ae792a686389f371a8a0320dfdd
Martin <hello@northrook.com>
2025-05-15 11:00:55 +0100

    mend

--- src/AssetManager/Config/RegisterAssetsPass.php
+++ src/AssetManager/Config/RegisterAssetsPass.php
@@ -4,46 +4,31 @@ declare(strict_types=1);
 
 namespace Core\AssetManager\Config;
 
-use Core\{AssetManager\Asset, AssetManager, AssetManager\AssetManifest};
+use Core\{AssetManager\Asset, AssetManager};
 use Core\AssetManager\Config\Asset as AssetAttribute;
 use Core\Symfony\Console\ListReport;
 use Core\Symfony\DependencyInjection\CompilerPass;
 use Support\Reflect;
-use Symfony\Component\Config\Loader\ParamConfigurator;
-use Symfony\Component\DependencyInjection\{ContainerBuilder, Definition, Parameter, Reference};
-use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
+use Symfony\Component\DependencyInjection\{ContainerBuilder, Definition, Reference};
 
 final class RegisterAssetsPass extends CompilerPass
 {
-    protected readonly Parameter $metaDirectory;
-
-    /**
-     * @param ParamConfigurator     $metaDirectory
-     * @param ReferenceConfigurator $manifest      {@see AssetManifest}
-     */
-    public function __construct(
-        ParamConfigurator               $metaDirectory,
-        protected ReferenceConfigurator $manifest,
-    ) {
-        $this->metaDirectory = new Parameter( (string) $metaDirectory );
-    }
-
     public function compile( ContainerBuilder $container ) : void
     {
-        $registeredServices = new ListReport( __METHOD__ );
+        $report = new ListReport( __METHOD__ );
 
-        $assetLocator  = $this->getDefinition( AssetManager::LOCATOR_ID );
-        $assetManifest = $this->getDefinition( AssetManifest::class );
+        $assetLocator = $this->getDefinition( AssetManager::LOCATOR_ID );
 
         $serviceLocatorArguments = [];
 
         foreach ( $this->taggedViewComponents() as $serviceId ) {
             //
-            $registeredServices->item( $serviceId );
+            $report->item( $serviceId );
 
             $serviceDefinition = $this->getDefinition( $serviceId, nullable : true );
+
             if ( $serviceDefinition === null ) {
-                $registeredServices->remove(
+                $report->remove(
                     $this::class." missing required '{$serviceId}' definition.",
                 );
 
@@ -53,44 +38,17 @@ final class RegisterAssetsPass extends CompilerPass
             $registeredAsset = $this->definitionViewComponentAttribute( $serviceDefinition );
 
             if ( $registeredAsset === null ) {
-                $registeredServices->error( $serviceId );
+                $report->error( $serviceId );
 
                 continue;
             }
 
-            // $meta = $registeredAsset->getAssetMeta( $this->metaDirectory );
-            // $assetManifest->addMethodCall( 'set', [$registeredAsset->className, $meta] );
-
             $serviceLocatorArguments[$serviceId] = new Reference( $serviceId );
         }
 
-        dump( $serviceLocatorArguments );
-
         $assetLocator->setArguments( [$serviceLocatorArguments] );
 
-        // $meta = new PhpStormMeta( $this->projectDirectory );
-        //
-        // $meta->registerArgumentsSet(
-        //     'view_component_keys',
-        //     ...\array_keys( $componentProperties ),
-        // );
-        //
-        // $generateReferences = \array_merge(
-        //     [
-        //         [ComponentBag::class, 'has'],
-        //         [ComponentBag::class, 'get'],
-        //         [ComponentFactory::class, 'render'],
-        //         [ComponentFactory::class, 'has'],
-        //     ],
-        // );
-        //
-        // foreach ( $generateReferences as $generateReference ) {
-        //     $meta->expectedArguments( $generateReference, [0 => 'view_component_keys'] );
-        // }
-        //
-        // $meta->save( 'view_components' );
-
-        $registeredServices->output();
+        $report->output();
     }
 
     /**
